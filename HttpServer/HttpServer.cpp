dbc36ffa63b95cfd9881d752085ea478b21e6155
Lee Ee Zian <eezian@vintedge.com>
2026-05-19 10:00:18 +0800

    init

---
+++ HttpServer/HttpServer.cpp
@@ -0,0 +1,12 @@
+﻿// HttpServer.cpp : Defines the entry point for the application.
+//
+
+#include "HttpServer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
