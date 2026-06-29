c7a2e9f354bcef963f4052dbbe233b9c71b8b5af
Lee Ee Zian <eezian@vintedge.com>
2026-06-29 17:47:35 +0800

    windows header guard

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,26 @@
 #include "Router.h"
 #include "repository/PokemonRepo.h"
 #include "controller/PokemonController.h"
+#include "AssetManager.h"
+#include "controller/UserController.h"
 #include "repository/UserRepo.h"
 #include "database/database.h"
 #include <asio.hpp>
 #include <fmt/color.h>
-#include <windows.h>
 
-#include "AssetManager.h"
-#include "controller/UserController.h"
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 int main() {
+#ifdef _WIN32
     // To display color in terminal
     const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
     DWORD dwMode = 0;
     GetConsoleMode(hOut, &dwMode);
     dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
     SetConsoleMode(hOut, dwMode);
-
-    // ------------------------------------
+#endif
     DataBase db{ "server.db" };
     asio::io_context io;
 
